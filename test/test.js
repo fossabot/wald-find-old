a99e00c93fd8289c3a0568b3bee91eb2851512bf
kuno <kuno@frob.nl>
2015-01-11 19:41:58 -0500

    Subqueries and more.

--- test/test.js
+++ test/test.js
@@ -9,8 +9,8 @@
 /* global suite test */
 
 var assert = require ('assert');
+var _ = require ('underscore');
 
-// var waldquery = require('../lib/query.js');
 var query = require ('../lib/query.js');
 
 var wq = query.connect ('ldf:http://localhost:4014/licensedb');
@@ -29,6 +29,24 @@ suite ('Query', function () {
             assert.equal (data.id, 'http://gnu.org/licenses/gpl-3.0.html');
             assert.equal (data.name, '"GNU General Public License"');
             assert.equal (data.sameAs[0], 'https://licensedb.org/id/GPL-3');
+        });
+    });
+
+    test ('subquery in array', function () {
+        var result = wq.query ('http://gnu.org/licenses/agpl-3.0.html', {
+            id: '@id',
+            name: 'dcterms:title',
+            replaces: [ wq.subquery ('dcterms:replaces', {
+                id: '@id',
+            }) ]
+        });
+
+        return result.then (function (data) {
+            var replaces = _(data.replaces).chain ().pluck ('id').sort ().value ();
+
+            assert.equal (data.id, 'http://gnu.org/licenses/agpl-3.0.html');
+            assert.equal (replaces[0], 'https://licensedb.org/id/AGPL-1');
+            assert.equal (replaces[1], 'https://licensedb.org/id/AGPL-2');
         });
     });
 
