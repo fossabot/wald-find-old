288d68cbb3befdbb8e3db9cf730b48c6a43e5e3d
kuno <kuno@frob.nl>
2015-01-11 20:44:49 -0500

    Normalize model + language filter.

--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ var assert = require ('assert');
 var _ = require ('underscore');
 
 var query = require ('../lib/query.js');
+var core = require ('../lib/core.js');
 
 var wq = query.connect ('ldf:http://localhost:4014/licensedb');
 
@@ -47,6 +48,37 @@ suite ('Query', function () {
             assert.equal (data.id, 'http://gnu.org/licenses/agpl-3.0.html');
             assert.equal (replaces[0], 'https://licensedb.org/id/AGPL-1');
             assert.equal (replaces[1], 'https://licensedb.org/id/AGPL-2');
+        });
+    });
+
+    test ('normalize', function () {
+        var result = wq.query ('http://gnu.org/licenses/agpl-3.0.html', {
+            id: '@id',
+            name: 'dcterms:identifier',
+            version: 'dcterms:hasVersion',
+            titles: [ 'dcterms:title' ],
+        }).then (core.normalizeModel);
+
+        return result.then (function (data) {
+            assert.equal (data.id, 'http://gnu.org/licenses/agpl-3.0.html');
+            assert.equal (data.name, 'GNU AGPL');
+            assert.equal (data.version, '3.0');
+            assert.equal (data.titles[0], 'GNU Affero General Public License');
+        });
+    });
+
+    test ('language filter', function () {
+        var result = wq.query ('http://creativecommons.org/licenses/by-sa/3.0/', {
+            id: '@id',
+            titles: [ 'dcterms:title' ],
+        }).then (core.language ('ko')).then (core.normalizeModel);
+
+        return result.then (function (data) {
+            var sorted = _(data.titles).sort ();
+
+            assert.equal (data.id, 'http://creativecommons.org/licenses/by-sa/3.0/');
+            assert.equal (sorted[0], 'Attribution-ShareAlike 3.0 Unported');
+            assert.equal (sorted[1], '저작자표시-동일조건변경허락 3.0 Unported');
         });
     });
 
